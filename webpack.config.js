daeb9577c61ce03a1f8f16a9b36c39632262f1f9
SeongJoon Kim <seongjoonkim@SeongJoonui-MacBookPro.local>
2021-04-27 00:17:31 +0900

    update react

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin"); // 웹팩이 html 파일을 읽어서 html 파일을 빌드할 수 있게
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/test.js", // 웹팩이 빌드할 파일
+  entry: "./src/index.js",
   output: { // 빌드를 완료하면 output 내에 명시되어있는 정보를 통해 빌드 파일 생성
     filename: "bundle.js",
     path: path.resolve(__dirname + "/build")
@@ -11,20 +12,32 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ['babel-loader'],
+      },
+      {
         test: /\.html$/,
         use: [
           {
             loader: "html-loader", // 웹팩이 이해할 수 있게하고
-            options: { minimize: true } // 코드 최적화 옵션.
+            options: { minimize: true } // 코드 최적화 옵션
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'] // 오른쪽 -> 왼쪽. css-loader로 css 파일을 읽고 MniCssExtractPlugin.loader로 읽은 CSS를 파일로 추출
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './public/index.html', // public/index.html 파일을 읽는다.
-      filename: 'index.html' // output으로 출력할 파일은 index.html이다.
+      filename: 'index.html' // output으로 출력할 파일은 index.html 이다.
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
     })
   ]
 };
